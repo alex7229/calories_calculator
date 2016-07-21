3a5b1a624c47a448755eb5d5d2a0396419e34b07
tup1tsa <tup1tsa@rambler.ru>
2016-07-21 20:39:39 +0300

    Add gulp/babel

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+﻿// Include gulp
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+// Include plugins
+/*var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');*/
+const rename = require('gulp-rename');
+
+gulp.task('scripts', function() {
+    return gulp.src('public/learnWords/*.js')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+});
+
+gulp.task('default', function () {
+    gulp.watch('public/learnWords/*.js', ['scripts']);
+});
+
